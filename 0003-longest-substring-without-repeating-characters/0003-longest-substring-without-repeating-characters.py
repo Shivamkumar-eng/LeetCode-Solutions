49f8d19874570bccf01ea69717f2f5e3309358f8
shivam kumar <sc333320@gmail.com>
2026-09-19 23:01:31 +0530

    Time: 174 ms (78.23%), Space: 20 MB (31.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,10 +3,10 @@ class Solution:
         left = 0
         d={}
         max_length=0
-        for right,char in enumerate(s):
-            if char in d and d[char]>=left:
-                left=d[char]+1
-            d[char]=right
+        for right in range(len(s)):
+            if s[right] in d and d[s[right]]>=left:
+                left=d[s[right]]+1
+            d[s[right]]=right
             current_length=right-left+1
             if current_length>=max_length:
                 max_length=current_length
